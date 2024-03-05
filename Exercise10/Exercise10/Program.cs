b531cc5e8e0689a4bf74cc7cd66314aa834bd837
inkwrenallen <145377496+inkwrenallen@users.noreply.github.com>
2024-03-05 13:19:52 -0500

    I am on step 4 of problem 1

---
+++ Exercise10/Exercise10/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Exercise10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+                //Createing a menu of choices for your user
+                Console.WriteLine("**************");
+                Console.WriteLine("");
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1 - New Game");
+                Console.WriteLine("2 - Load Game");
+                Console.WriteLine("3 - Options");
+                Console.WriteLine("4 - Quit");
+                Console.WriteLine("**************");
+
+                //prompting for and reading in the user’s choice as an integer
+                Console.WriteLine("please make a selection:");
+                int userChoice = int.Parse(Console.ReadLine());
+            //while loop
+            while (userChoice == 4)
+            {
+               
+                //application printing an appropriate response confirming their choice
+                if (userChoice == 1)
+                {
+                    Console.WriteLine("Loading New Game ...");
+                }
+                if (userChoice == 2)
+                {
+                    Console.WriteLine("Loading Game ...");
+                }
+                if (userChoice == 3)
+                {
+                    Console.WriteLine("Loading Options ...");
+                }
+                if (userChoice == 4)
+                {
+                    Console.WriteLine("Quiting game ...");
+                }
+                Console.WriteLine("**************");
+                Console.WriteLine("");
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1 - New Game");
+                Console.WriteLine("2 - Load Game");
+                Console.WriteLine("3 - Options");
+                Console.WriteLine("4 - Quit");
+                Console.WriteLine("**************");
+                Console.WriteLine("please make a selection:");
+            }
+        }
+    }
+}
